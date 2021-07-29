3a176bfd02a02ff3cede0903a9f3b571b5e6496d
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2021-07-29 15:54:30 +0300

    ready

---
+++ src/js/app.js
@@ -0,0 +1,53 @@
+import menuCardTpl from '../templates/menu-card.hbs';
+import menu from '../menu.json';
+
+const menuCards = document.querySelector('.js-menu');
+
+const markup = menuCardTpl(menu);
+menuCards.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+  
+
+const themeSwitchControl = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+  
+  
+body.classList.add(Theme.LIGHT);
+  
+setLocalStorage();
+  
+themeSwitchControl.addEventListener('change', changeSwitchControl);
+  
+  
+  
+  
+function changeSwitchControl() {
+  
+const check = themeSwitchControl.checked;
+  
+if (check) {
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem("theme", "dark-theme");
+} else {
+    body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem("theme", "light-theme");
+    }
+}
+  
+function setLocalStorage() {
+  
+const getTheme = localStorage.getItem("theme");
+  
+if (getTheme === 'dark-theme') {
+    body.classList.add(Theme.DARK);
+    themeSwitchControl.checked = true;
+    localStorage.setItem("theme", "dark-theme");
+  
+} else {
+    localStorage.setItem("theme", "light-theme");
+    }
+}
